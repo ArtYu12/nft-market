79de5352fb26f565c363fb2f6dda30e54f9bd981
ArtYu12 <korzaevmatvej@gmail.com>
2024-05-12 17:51:44 +0600

    directives/routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Main from "./pages/main";
 
 
 function App() {
+  const [userInfo,setUserInfo] = useState(null)
+
+  useEffect(() => {
+
+  },{})
   return (
     <div className="app">
-      
+      <Routes>
+        <Route path="/" element={<Main />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/main.jsx
@@ -0,0 +1,11 @@
+
+
+const Main = () => {
+    return (
+        <div className="main__page">
+            main
+        </div>
+    )
+}
+
+export default Main
